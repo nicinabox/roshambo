5eb4b88a289249e7879b7d47ed06770a8c22a6ba
Nic Aitch <nic@nicinabox.com>
2014-10-22 00:01:48 -0500

    Update game to stay in loop

--- index.js
+++ index.js
@@ -1,24 +1,34 @@
 var Roshambo = require('./roshambo');
 var inquirer = require('inquirer');
 var _ = require('lodash');
+var colors = require('colors');
 
 (function() {
   'use strict';
   var shapes = Roshambo.prototype.shapes;
 
-  inquirer.prompt({
-    type: 'list',
-    message: 'Ready?',
-    name: 'shape',
-    choices: shapes
-  }, function(answers) {
-    var shape = _.indexOf(shapes, answers.shape);
-    var rps = new Roshambo(shape);
+  var play = function() {
+    inquirer.prompt({
+      type: 'list',
+      message: 'Ready?',
+      name: 'shape',
+      choices: shapes
+    }, function(answers) {
+      result(answers.shape);
+      _.delay(play, 1500);
+    });
+  };
+
+  var result = function(shape) {
+    var index = _.indexOf(shapes, shape);
+    var rps   = new Roshambo(index);
 
     console.log('You played', rps.shapes[rps.playerA]);
     console.log('They played', rps.shapes[rps.playerB]);
 
-    console.log('>', rps.outcome());
-  });
+    var colors = ['gray', 'green', 'red'];
+    console.log('> ' + rps.outcome()[colors[rps.result]]);
+  };
 
+  play();
 })();
